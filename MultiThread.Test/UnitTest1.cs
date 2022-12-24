e32429546a26f478266519ff0967df355aacaef4
Ali Ertunç <ertuncali07@gmail.com>
2022-12-24 15:07:31 +0300

    First Commit

---
+++ MultiThread.Test/UnitTest1.cs
@@ -0,0 +1,144 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace MultiThread.Test
+{
+    //[TestClass]
+    //public class UnitTest1
+    //{
+    //    [TestMethod]
+    //    public void TestLoadList()
+    //    {
+    //        // LoadList metodunu çalýþtýr ve objeler listesini al
+    //        Program.LoadList();
+    //        List<object[]> objects = Program.objects;
+
+    //        // Test: objeler listesi doðru bir þekilde yüklenmiþ mi?
+    //        Assert.AreEqual(100, objects.Count);
+    //        for (int i = 0; i < 100; i++)
+    //        {
+    //            Assert.AreEqual(i, (int)objects[i][0]);
+    //            Assert.AreEqual($"Name{i}", (string)objects[i][1]);
+    //            Assert.AreEqual($"Surname{i}", (string)objects[i][2]);
+    //            Assert.AreEqual($"01/01/19{i % 100}", (string)objects[i][3]);
+    //            Assert.AreEqual("Status", (string)objects[i][4]);
+    //        }
+    //    }
+
+    //    [TestMethod]
+    //    public void TestCreatePdf()
+    //    {
+    //        // LoadList metodunu çalýþtýr ve objeler listesini al
+    //        Program.LoadList();
+    //        List<object[]> objects = Program.objects;
+
+    //        // PDF dosyalarýný oluþtur
+    //        Program.CreatePdf();
+
+    //        // Test: PDF dosyalarý doðru bir þekilde oluþturulmuþ mu?
+    //        foreach (object[] obj in objects)
+    //        {
+    //            int id = (int)obj[0];
+    //            string fileName = $"object_{id}.pdf";
+    //            string filePath = Path.Combine(Program.pdfPath, fileName);
+    //            Assert.IsTrue(File.Exists(filePath));
+    //        }
+    //    }
+    //}
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        private ProgramFixture _fixture;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fixture = new ProgramFixture();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _fixture.Dispose();
+        }
+
+        [TestMethod]
+        public void TestLoadList()
+        {
+            // objeler listesini al
+            List<object[]> objects = _fixture.objects;
+
+            // Test: objeler listesi doðru bir þekilde yüklenmiþ mi?
+            Assert.AreEqual(100, objects.Count);
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.AreEqual(i, (int)objects[i][0]);
+                Assert.AreEqual($"Name{i}", (string)objects[i][1]);
+                Assert.AreEqual($"Surname{i}", (string)objects[i][2]);
+                Assert.AreEqual($"01/01/19{i % 100}", (string)objects[i][3]);
+                Assert.AreEqual("Status", (string)objects[i][4]);
+            }
+        }
+
+        [TestMethod]
+        public void TestCreatePdf()
+        {
+            // objeler listesini al
+            List<object[]> objects = _fixture.objects;
+
+            // PDF dosyalarýný oluþtur
+            _fixture.CreatePdf();
+
+            // Test: PDF dosyalarý doðru bir þekilde oluþturulmuþ mu?
+            foreach (object[] obj in objects)
+            {
+                int id = (int)obj[0];
+                string fileName = $"object_{id}.pdf";
+                string filePath = Path.Combine(_fixture.pdfPath, fileName);
+                Assert.IsTrue(File.Exists(filePath));
+            }
+        }
+    }
+
+    public class ProgramFixture : IDisposable
+    {
+        public List<object[]> objects { get; private set; }
+        public string pdfPath { get; private set; }
+
+        public ProgramFixture()
+        {
+            // LoadList metodunu çalýþtýr ve objeler listesini al
+            Program.LoadList();
+            objects = Program.objects;
+            pdfPath = Program.pdfPath;
+        }
+
+        public void CreatePdf()
+        {
+            // PDF dosyalarýný oluþtur
+            Program.CreatePdf();
+        }
+
+        public void Dispose()
+        {
+            // PDF dosyalarýný sil
+            foreach (object[] obj in objects)
+            {
+                int id = (int)obj[0];
+                string fileName = $"object_{id}.pdf";
+                string filePath = Path.Combine(pdfPath, fileName);
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+            }
+        }
+
+    }
+}
+
---
+++ MultiThread/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+namespace MultiThread
+{
+    public class Program
+    {
+        public static List<object[]> objects;
+        public static string pdfPath = @"C:\Users\ali\source\repos\MultiThread\MultiThread\PdfPath";
+
+        public static void Main(string[] args)
+        {
+            LoadList();
+            CreatePdf();
+            Console.ReadLine();
+        }
+        public static void LoadList()
+        {
+            // Fake objeler listesi ve her bir objenin detayları
+            objects = new List<object[]>();
+            for (int i = 0; i < 100; i++)
+            {
+                objects.Add(new object[] { i, $"Name{i}", $"Surname{i}", $"01/01/19{i % 100}", "Status" });
+            }
+        }
+        public static void CreatePdf()
+        {
+            CheckPath();
+            // Threads listesi
+            List<Thread> threads = new List<Thread>();
+
+            foreach (object[] obj in objects)
+            {
+                // Objenin detaylarını al
+                int id = (int)obj[0];
+                string name = (string)obj[1];
+                string surname = (string)obj[2];
+                string bdate = (string)obj[3];
+                string status = (string)obj[4];
+
+                Thread thread = new Thread(() =>
+                {
+                    // PDF dosyasını oluştur ve objenin detaylarını içine yaz
+                    string fileName = $"object_{id}.pdf";
+                    string filePath = Path.Combine(pdfPath, fileName);
+
+                    using (FileStream stream = new FileStream(filePath, FileMode.Create))
+
+                    {
+                        Document document = new Document();
+                        PdfWriter.GetInstance(document, stream);
+                        document.Open();
+
+                        document.Add(new Paragraph($"ID: {id}"));
+                        document.Add(new Paragraph($"Name: {name}"));
+                        document.Add(new Paragraph($"Surname: {surname}"));
+                        document.Add(new Paragraph($"Birth date: {bdate}"));
+                        document.Add(new Paragraph($"Status: {status}"));
+
+                        document.Close();
+                    }
+                    Console.WriteLine(filePath);
+                });
+
+                thread.Priority = ThreadPriority.Lowest;
+                thread.Start();
+                threads.Add(thread);
+
+                if (threads.Count == 5)
+                {
+                    threads[0].Join();
+                    threads.RemoveAt(0);
+                }
+                Console.WriteLine(threads.Count.ToString());
+            }
+        }
+        public static void CheckPath()
+        {
+            // Dizin yoksa oluştur
+            if (!Directory.Exists(pdfPath))
+            {
+                Directory.CreateDirectory(pdfPath);
+            }
+        }
+    }
+}
+
